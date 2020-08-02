824456fb0b15d448664de78717ac571be6eb7933
Matt <matthewkeehner@gmail.com>
2020-08-02 02:40:13 -0500

    added light/dark toggle based on device pref and gem click

--- main.js
+++ main.js
@@ -1,9 +1,44 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('sw.js')
   .then((registration) => {
-    console.log('yee', registration);
+    // console.log('yee', registration);
   })
   .catch((error) => {
     console.log('nee', error);
   });
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  var doc = document.querySelector('html');
+
+  if (localStorage.getItem('use-dark-color-scheme') === null) {
+    if (window.matchMedia) {
+      localStorage.setItem('use-dark-color-scheme', window.matchMedia("(prefers-color-scheme: dark)").matches);
+    } else {
+      localStorage.setItem('use-dark-color-scheme', 'true');
+    }
+  }
+
+  doc.setAttribute('dark', localStorage.getItem('use-dark-color-scheme'));
+
+  window.matchMedia('(prefers-color-scheme: dark)')
+  .addEventListener('change', e => {
+    doc.setAttribute('dark', e.matches);
+  });
+
+  var penpenObject = document.getElementById('penpen');
+  penpenObject.addEventListener('load', () => {
+    var penpen = penpenObject.contentDocument;
+    var gem = penpen.getElementById('gem');
+
+    gem.addEventListener('click', () => {
+      if (doc.getAttribute('dark') === 'true') {
+        localStorage.setItem('use-dark-color-scheme', 'false');
+      } else {
+        localStorage.setItem('use-dark-color-scheme', 'true');
+      }
+
+      doc.setAttribute('dark', localStorage.getItem('use-dark-color-scheme')) ;
+    }, false);
+  }, false);
+});
